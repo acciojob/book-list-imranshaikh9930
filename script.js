f114a84b0c736f231cdc21618322c6c8dd6f8aef
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-09-22 20:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const titleVal = document.getElementById("title");
+const authorVal = document.getElementById("author");
+const isbnVal = document.getElementById("isbn");
+const btn = document.getElementById("submit");
+const tbody = document.getElementById("book-list");
+
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+    let temp = "";
+    temp +=`<tr>
+                <td>${titleVal.value}</td>
+                <td>${authorVal.value}</td>
+                <td>${isbnVal.value}</td>
+                <td class="delete">X</td>
+                </tr>`;
+
+    console.log(tbody);
+
+    tbody.innerHTML += temp;
+	
+})
+
+const table = document.querySelector(".table-striped");
+
+table.addEventListener("click",(e)=>{
+
+    if(e.target && e.target.classList.contains("delete")){
+        const row = e.target.closest("tr");
+        row.remove();
+    }
+
+})
+
